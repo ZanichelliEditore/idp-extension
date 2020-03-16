e4c46239b404b5b7f8bf936e8dcb3f5097cec4e6
Michela Tarozzi <mtarozzi@zanichelli.it>
2020-03-16 09:06:43 +0100

    added migration and model for grant, added idpAuthMiddleware

---
+++ src/Models/ZGrant.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Zanichelli\IdpExtension\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ZGrant extends Model
+{
+    public $timestamps = false;
+}
--- src/Providers/IdpProvider.php
+++ src/Providers/IdpProvider.php
@@ -11,7 +11,11 @@ class IdpProvider implements ServiceProvider
      */
     public function register()
     {
-        //
+        Auth::extend('idp', function ($app, $name, array $config) {
+            return tap($this->makeGuard($config), function ($guard) {
+                $this->app->refresh('request', $guard, 'setRequest');
+            });
+        });
     }
 
     /**
@@ -21,6 +25,20 @@ class IdpProvider implements ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->publishes([
+            __DIR__ . '/../../database/migrations' => database_path('/migrations'),
+        ], 'database');
+    }
+
+    /**
+     * Register Idp's migration files.
+     *
+     * @return void
+     */
+    protected function registerMigrations()
+    {
+        if (IdpExtension::$runsMigrations) {
+            return $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        }
     }
 }
