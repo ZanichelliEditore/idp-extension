50fc378aeb524527a849e53419e98c6354b9c68f
Michela Tarozzi <mtarozzi@zanichelli.it>
2025-05-05 16:20:55 +0200

    BTH-6678: create JWKS Middleware

--- src/Http/Middleware/IdpApiMiddleware.php
+++ src/Http/Middleware/IdpApiMiddleware.php
@@ -2,50 +2,24 @@
 
 namespace Zanichelli\IdpExtension\Http\Middleware;
 
-use Carbon\Carbon;
 use Closure;
 use Exception;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-use Firebase\JWT\JWT;
-use Firebase\JWT\JWK;
-use Illuminate\Support\Facades\Cache;
 
 class IdpApiMiddleware
 {
-    private const CACHE_KEY = 'public_key';
+    protected $client;
 
-    public function handle(Request $request, Closure $next, string $withV1User = 'with_v1_user')
+    public function handle(Request $request, Closure $next)
     {
-        $token = $request->input('token') ?? $request->cookies->get(config("idp.cookie.name")) ?? $request->headers->get('token');
+        $this->client = new Client(['base_uri' => env('IDP_BASE_URL'), 'verify' => false]);
 
+        $token = $request->input('token') ?? $request->cookies->get(config("idp.cookie.name")) ?? $request->headers->get('token');
         if ($token) {
             try {
-                $client = new Client(['verify' => false]);
-                if ($withV1User === 'with_v1_user') {
-                    $res = $client->get(env('IDP_BASE_URL') . '/v1/user?token=' . $token);
-                    $user = json_decode($res->getBody(), true);
-                } else {
-                    if (!$jwk = Cache::store('file')->get(self::CACHE_KEY)) {
-                        $res = $client->get(env('IDP_BASE_URL') . '/.well-known/jwks.json');
-                        $jwk = json_decode($res->getBody());
-                        Cache::store('file')->put(self::CACHE_KEY, $jwk);
-                    }
-
-                    $user = (array) JWT::decode($token, JWK::parseKey((array) $jwk->keys[0]));
-                }
-
-                if ($withV1User !== 'with_v1_user') {
-                    $user['name'] = $user['given_name'];
-                    $user['surname'] = $user['family_name'];
-                    $user['username'] = $user['preferred_username'];
-                    $user['roles'] = $this->getFormattedRoles($user['roles']);
-                    $user['attributes'] = $this->getFormattedAttributes($user['attributes']);
-                    $user['myz'] = (array) $user['myz'];
-
-                    unset($user['iat'], $user['exp'], $user['nbf'], $user['sub'], $user['prv'], $user['given_name'], $user['family_name'], $user['preferred_username']);
-                }
+                $user = $this->getUser($token);
 
                 $request->merge(['user' => $user]);
             } catch (Exception $e) {
@@ -59,30 +33,10 @@ class IdpApiMiddleware
         return $next($request);
     }
 
-    protected function getFormattedRoles(array $roles): array
+    protected function getUser($token)
     {
-        return array_map(function ($role) {
-            return [
-                "id" => $role->id,
-                "roleId" => $role->role_id,
-                "roleName" => $role->roleName,
-                "departmentId" => $role->department_id,
-                "departmentName" => null,
-                "branchCode" => null,
-            ];
-        }, $roles);
-    }
-
-    protected function getFormattedAttributes(object $attributes): array
-    {
-        $parsedAttributes = [];
-
-        foreach ($attributes as $key => $value) {
-            $parsedAttributes[] = [
-                "key" => $key,
-                "value" => $value
-            ];
-        }
-        return $parsedAttributes;
+        $res = $this->client->get('/v1/user?token=' . $token);
+        $user = json_decode($res->getBody(), true);
+        return $user;
     }
 }
