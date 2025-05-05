dfc542dcdca30c41c4f5f90283ac905860b0d68e
Veronica Vannini <vvannini@zanichelli.it>
2025-05-05 14:26:02 +0200

    BTH-6678: remap $user

--- src/Http/Middleware/IdpApiMiddleware.php
+++ src/Http/Middleware/IdpApiMiddleware.php
@@ -42,13 +42,15 @@ class IdpApiMiddleware
                     $user = (array) JWT::decode($token, JWK::parseKey((array) $jwk->keys[0]));
                 }
 
-                $user['isVerified'] = $user['is_verified'];
-                $user['isEmployee'] = $user['is_employee'];
-                $user['createdAt'] = $user['created_at'];
-                unset($user['is_verified'], $user['is_employee'], $user['created_at']);
-
                 if ($withV1User !== 'with_v1_user') {
-                    unset($user['iat'], $user['exp'], $user['nbf'], $user['sub'], $user['prv']);
+                    $user['name'] = $user['given_name'];
+                    $user['surname'] = $user['family_name'];
+                    $user['username'] = $user['preferred_username'];
+                    $user['roles'] = $this->getFormattedRoles($user['roles']);
+                    $user['attributes'] = $this->getFormattedAttributes($user['attributes']);
+                    $user['myz'] = (array) $user['myz'];
+
+                    unset($user['iat'], $user['exp'], $user['nbf'], $user['sub'], $user['prv'], $user['given_name'], $user['family_name'], $user['preferred_username']);
                 }
 
                 $request->merge(['user' => $user]);
@@ -61,5 +63,32 @@ class IdpApiMiddleware
         }
 
         return $next($request);
+    }
+
+    protected function getFormattedRoles(array $roles): array
+    {
+        return array_map(function ($role) {
+            return [
+                "id" => $role->id,
+                "roleId" => $role->role_id,
+                "roleName" => $role->roleName,
+                "departmentId" => $role->department_id,
+                "departmentName" => null,
+                "branchCode" => null,
+            ];
+        }, $roles);
+    }
+
+    protected function getFormattedAttributes(object $attributes): array
+    {
+        $parsedAttributes = [];
+
+        foreach ($attributes as $key => $value) {
+            $parsedAttributes[] = [
+                "key" => $key,
+                "value" => $value
+            ];
+        }
+        return $parsedAttributes;
     }
 }
